93cc367fd63e6c146aede30992adb2376e69ab66
Abhishekj9621 <aj962143@gmail.com>
2026-06-18 13:01:04 -0400

    updated home

--- assets/theme.js
+++ assets/theme.js
@@ -22,3 +22,20 @@ threshold:.2
 
 document.querySelectorAll(".fade-up")
 .forEach((el)=>observer.observe(el));
+
+
+const header = document.querySelector('.site-header');
+
+window.addEventListener('scroll',()=>{
+
+if(window.scrollY > 50){
+
+header?.classList.add('scrolled');
+
+}else{
+
+header?.classList.remove('scrolled');
+
+}
+
+});
